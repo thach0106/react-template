b6c475c83751aa1e32849f366a2e5803123fe87e
Thach Da <thachbovjp@gmail.com>
2023-04-08 14:53:19 +0700

    config vite eslint and  prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      hello!
-    </div>
-  )
+  return <div className="App">hello!</div>;
 }
 
-export default App
+export default App;

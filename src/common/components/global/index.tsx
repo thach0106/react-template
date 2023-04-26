4b586b2a1513a74f92d903838523a0fed36a6999
ngoc thach <thachnn@icd-vn.com>
2023-04-26 17:04:14 +0700

    define theme for app

--- src/common/components/global/index.tsx
+++ src/common/components/global/index.tsx
@@ -1,19 +1,23 @@
 import React, { ReactNode, Fragment } from "react";
 import { globalStyles } from "./global.styles";
 import { Global } from "@emotion/react";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
-import "normalize.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/common/styles/scss/_common.scss";
 
 interface PropsType {
   children: ReactNode;
 }
+const theme = createTheme({
+  // Override or create new styles, colors, palettes...
+});
 
 const GlobalStyles: React.FC<PropsType> = ({ children }) => {
   return (
     <Fragment>
-      {children}
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      <CssBaseline enableColorScheme />
       <Global styles={globalStyles} />
     </Fragment>
   );
